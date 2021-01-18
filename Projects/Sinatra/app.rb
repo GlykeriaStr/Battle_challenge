09be06a317f4e8cfdc8905de77c65e0924f33000
Reeshul <reeshul@hotmail.com>
2021-01-18 16:33:40 +0000

    Moved HTML from app to erb file

--- Projects/Sinatra/app.rb
+++ Projects/Sinatra/app.rb
@@ -13,7 +13,5 @@ get '/secret4' do
 end
 
 get '/dog' do
-  "<div style ='border: 3px dashed red' >
-  <img src='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F09%2F22%2F50-cute-dog-names.jpg'/>
-  </div>"
+  erb(:index)
 end
